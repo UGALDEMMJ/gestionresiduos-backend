107663ac4c7619990137f14c03064ce5316d6369
UGALDEMMJ <UgaldeMoralesMJ@gmail.com>
2025-07-27 16:13:16 -0600

    .

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
+    origin: process.env.FRONTEND_URL, 
     credentials: true
 }))
 
