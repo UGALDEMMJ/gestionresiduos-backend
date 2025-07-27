223a6eb84f35233dede5d19afeae70076e89c04d
UGALDEMMJ <UgaldeMoralesMJ@gmail.com>
2025-07-27 15:13:33 -0600

    Configuracion de produccion

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import cors from 'cors';
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.FRONTEND_URL || "http://localhost:3000", 
     credentials: true
 }))
 
